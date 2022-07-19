0a06d4788dbe7f2f2fb6021ec942d79e6107a89d
rmdhnprmd <ramadhan.permadi36@gmail.com>
2022-07-19 16:56:16 +0700

    nambain klik again

--- script.js
+++ script.js
@@ -22,9 +22,7 @@ console.log(document.querySelector('.guess').value);
 
 // use global event target
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector('.check').onclick = function () {
@@ -38,6 +36,8 @@ document.querySelector('.check').onclick = function () {
     // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -64,4 +64,17 @@ document.querySelector('.check').onclick = function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+};
+
+document.querySelector('.again').onclick = () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start gussing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 };
